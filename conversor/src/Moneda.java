9583fe588c4b20f8e135987bf94e8356320335a0
William Atuesta <w.atuesta@gmail.com>
2024-05-14 23:30:33 -0500

    Se finaliza el desarrollo para la conversión de monedas con la API Exchange Rate, se da por finalizado el challenge

--- conversor/src/Moneda.java
+++ conversor/src/Moneda.java
@@ -1,14 +1,4 @@
 import java.util.Map;
 
-public record Moneda(String result,
-                        String documentation,
-                        String terms_of_use,
-                        long time_last_update_unix,
-                        String time_last_update_utc,
-                        long time_next_update_unix,
-                        String time_next_update_utc,
-                        String base_code,
-                        String target_code,
-                        Double conversion_rate,
-                        Double conversion_result) {
+public record Moneda(String conversion_result) {
 }
--- conversor/src/Principal.java
+++ conversor/src/Principal.java
@@ -4,19 +4,62 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) throws IOException {
 
+        String monedaAConsultar = "";
+        String monedaDestino = "";
+        Integer opc = 999;
         Scanner lectura = new Scanner(System.in);
-        ConsultaMoneda consulta = new ConsultaMoneda();
-        System.out.println("Escriba la moneda a convertir: ");
-        var monedaAConsultar = lectura.nextLine();
 
-        System.out.println("Moneda destino: ");
-        var monedaDestino = lectura.nextLine();
+        while (opc != 0) {
+            System.out.println("\n        BIENVENID@ AL CONVERSOR DE MONEDAS - (2024) - |ExchangeRate-API|\n");
+            System.out.println("1) Dolar(USD)           ---> Peso Colombiano(COP)");
+            System.out.println("2) Peso Argentino(ARS)  ---> Dolar(USD)");
+            System.out.println("3) Boliviano(BOB)       ---> Dolar(USD)");
+            System.out.println("4) Peso Colombiano(COP) ---> Dolar(USD)");
+            System.out.println("5) Peso Chileno(CLP)    ---> Dolar(USD)");
+            System.out.println("6) Real Brasileño(BRL)  ---> Dolar(USD)");
+            System.out.println("0) Salir\n");
+            System.out.println("Elija qué tipo de conversión desea hacer: ");
 
-        System.out.println("Valor a convertir");
-        var valorAConvertir = Double.valueOf(lectura.nextLine());
-        Moneda moneda = consulta.buscaMoneda(monedaAConsultar, monedaDestino, valorAConvertir);
-        System.out.println(moneda);
+            opc = lectura.nextInt();
 
-
+            switch (opc) {
+                case 1:
+                    monedaAConsultar = "USD";
+                    monedaDestino = "COP";
+                    break;
+                case 2:
+                    monedaAConsultar = "ARS";
+                    monedaDestino = "USD";
+                    break;
+                case 3:
+                    monedaAConsultar = "BOB";
+                    monedaDestino = "USD";
+                    break;
+                case 4:
+                    monedaAConsultar = "COP";
+                    monedaDestino = "USD";
+                    break;
+                case 5:
+                    monedaAConsultar = "CLP";
+                    monedaDestino = "USD";
+                    break;
+                case 6:
+                    monedaAConsultar = "BRL";
+                    monedaDestino = "USD";
+                    break;
+                case 0:
+                    System.out.println("\nGracias por utilizar el conversor.");
+                    break;
+            }
+            if (opc > 0 && opc < 7){
+                ConsultaMoneda consulta = new ConsultaMoneda();
+                System.out.println("Valor a convertir");
+                var valorAConvertir = Double.valueOf(lectura.nextDouble());
+                Moneda moneda = consulta.buscaMoneda(monedaAConsultar, monedaDestino, valorAConvertir);
+                System.out.println("$"+valorAConvertir+monedaAConsultar+" son: $"+moneda.conversion_result()+monedaDestino);
+            }else if(opc != 0 && opc > 6) {
+                System.out.println("Ingrese una opción válida =>");
+            }
+        }
     }
 }
